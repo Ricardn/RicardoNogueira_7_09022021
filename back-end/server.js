aa76e01019d1ec06ca625cba7559dc32e15f979b
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-07-29 12:50:29 +0200

    [Add] Back-end
    Create ServerJS, AppJs, User Route and User Controller

---
+++ back-end/server.js
@@ -0,0 +1,52 @@
+//Create http server by calling the package http and require ./app file.
+const http = require("http");
+const app = require("./app");
+
+//Return a valid port whether supplied as a number or as a string.
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+//Search for various errors, handles them and save them on the server.
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+//Event listener logging the port or named pipe from the server running and add a message in the console.
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
+
+server.listen(port);
