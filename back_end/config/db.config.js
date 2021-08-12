1df15147f29dc561ce5e29c0e40fb181cc4ce279
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-08-12 12:46:41 +0200

    [Add] DataBase Configuration

---
+++ back_end/config/db.config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
+  dialect: "mysql",
+  //used for Sequelize connection pool configuration
+  pool: {
+    max: 5, //maximum number of connection in pool
+    min: 0, //minimum number of connection in pool
+    acquire: 30000, //maximum time, in milliseconds, that pool will try to get connection before throwing error
+    idle: 10000, //maximum time, in milliseconds, that a connection can be idle before being released
+  },
+};
