d323704060753ec606a7bb7afd740676e9230447
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-08-12 12:49:52 +0200

    [Update] SignIn and SignUp Pages

--- front-end/src/pages/sign-in/sign-in.jsx
+++ front-end/src/pages/sign-in/sign-in.jsx
@@ -1,45 +1,38 @@
 import React from "react";
 import "./sign-in.scss";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+
+import TextField from "@material-ui/core/TextField";
+import Nav from "../../components/header/logPage/index";
+import SignInbtn from "../../components/buttons/btn-signin/index"
 
 export default function SignIn() {
   return (
     <div>
-      <nav>
-        <div className="topbarContainer">
-          <div className="topbarLeft">
-            <div className="logo-container">
-              <a href="#">
-                <div className="topbarLogo">
-                  <img src="/images/logo-white.png" alt="logo" />
-                  <h1 className="logoName">Groupomania</h1>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div className="topbarRight">
-            <div className="btn-container">
-              <div className="btn-return">
-                <a className="return" href="#">
-                  <ArrowBackIcon className="returnIcon" />
-                  Retour en arrière
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Nav />
       <div className="main">
         <div className="information-container">
           <h2>Nous sommes ravis de vous revoir,</h2>
           <h3>
             identifiez-vous afin de retrouver votre communauté professionnelle.
           </h3>
+          <div className="input-container">
+            <TextField
+              className="emailInput"
+              id="outlined-password-input"
+              label="Email"
+              type="email"
+              variant="outlined"
+            />
+            <TextField
+              className="passwordInput"
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              variant="outlined"
+            />
+          </div>
           <div className="sign-in-Container">
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Mot de Passe" />
-            <a href="#">Mot de passe oublié ?</a>
-            <button>S'identifier</button>
+            <SignInbtn className="signBtn"/>
           </div>
         </div>
       </div>
--- front-end/src/pages/sign-up/sign-up.jsx
+++ front-end/src/pages/sign-up/sign-up.jsx
@@ -1,63 +1,111 @@
-import React from "react";
-import "./sign-up.scss";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { React, useState, useEffect } from "react";
+import Nav from "../../components/header/logPage/index";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import { styled } from "@material-ui/core/styles";
+import Button from "../../components/buttons/btn-validation/index";
+import { Link } from "@material-ui/core";
+import './style.scss';
+
+async function registerUser(credentials) {
+  return fetch("http://localhost:3000/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
 
 export default function SignUp() {
+  //Signup
+  const [userGenderReg, setuserGenderReg] = useState("");
+  const [userlastNameReg, setuserLastnameReg] = useState("");
+  const [userfirstNameReg, setUserNameReg] = useState("");
+  const [userEmailReg, setUserEmailReg] = useState("");
+  const [userPasswordReg, setUserPasswordReg] = useState("");
+
+  const register = () => {
+    console.log(userlastNameReg);
+  }
+
+
+
+
   return (
     <div>
-      <nav>
-        <div className="topbarContainer">
-          <div className="topbarLeft">
-            <div className="logo-container">
-              <a href="#">
-                <div className="topbarLogo">
-                  <img src="/images/logo-white.png" alt="logo" />
-                  <h1 className="logoName">Groupomania</h1>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div className="topbarRight">
-            <div className="btn-container">
-              <div className="btn-return">
-                <a className="return" href="#">
-                  <ArrowBackIcon className="returnIcon" />
-                  Retour en arrière
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Nav />
       <div className="main">
         <div className="information-container">
-          <h2>Nous sommes ravis de vous revoir,</h2>
-          <h3>
-            identifiez-vous afin de retrouver votre communauté professionnelle.
-          </h3>
-          <div className="sign-in-Container">
-            <div>
-              <input type="text" placeholder="Nom" />
-              <input type="text" placeholder="Prénom" />
-              <button>Continuer</button>
-            </div>
+          <h2>Créer votre compte Groupomania,</h2>
+          <h3>n'a jamais été aussi simple et rapide.</h3>
+          <div className="input-container">
+            <form>
+              <div className="form-container">
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Genre</FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="gender"
+                    name="row-radio-buttons-group"
+                  >
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio />}
+                      label="Femme"
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio />}
+                      label="Homme"
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={<Radio />}
+                      label="Autre"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </div>
+              <TextField
+                className="surnameInput"
+                id="outlined-surname-input"
+                label="Nom"
+                type="text"
+                variant="outlined"
+              />
 
-            <div>
-              <input type="text" placeholder="Email" />
-              <input type="text" placeholder="Mot de Passe" />
-              <button>Continuer</button>
-            </div>
+              <TextField
+                className="nameInput"
+                id="outlined-name-input"
+                label="Prénom"
+                type="text"
+                variant="outlined"
+              />
 
-            <div>
-              <h2></h2>
-              <input type="file"></input>
-              <a href="#">Choisir une photo de profil plus tard</a>
-              <button>Continuer</button>
-            </div>
+              <TextField
+                className="emailInput"
+                id="outlined-email-input"
+                label="Email"
+                type="email"
+                variant="outlined"
+              />
+              <TextField
+                className="passInput"
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                variant="outlined"
+              />
+              <Link onClick={() => console.log("Click")}>
+                <Button />
+              </Link>
+            </form>
           </div>
         </div>
       </div>
-      <footer></footer>
     </div>
   );
 }
