51f1a584fd1d92635878ba4384466a442aff09cb
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-08-12 12:48:53 +0200

    [Update] App JS

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,42 +3,24 @@ import React from "react";
 import Home from "./pages/home/Home.jsx";
 import SignIn from "./pages/sign-in/sign-in.jsx";
 import SignUp from "./pages/sign-up/sign-up.jsx";
-
-import NavBar from "./components/topbar/Topbar.jsx";
-import Feed from "./pages/feed/feed.jsx";
+import Feed from './pages/feed/feed.jsx';
 
 
-import { HashRouter,Route,  Switch, } from "react-router-dom";
 
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/feed" component={Feed} />
-        </Switch>
-      </div>
-    </HashRouter>
+    <Router>
+      <Switch>
+        <Route exact path="/feed" component={Feed} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
-
-
-//<Router>
-//     <Switch>
-//        <Route path="/" extact component={Home}>
-//        </Route>
-//        <Route path="/signin" component={SignIn}>
-//        </Route>
-//        <Route path="/signup" component={SignUp}>
-//       </Route>
-//      </Switch>
-//    </Router>
-
 
 
 export default App;
