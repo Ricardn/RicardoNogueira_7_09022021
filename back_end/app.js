798a9b585703692aa9dad34e86adedc9022f6868
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-08-12 12:46:23 +0200

    [Update] BackEnd

---
+++ back_end/app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const path = require("path");
+const mysql = require('mysql2');
+
+const db = require('./models/index');
+
+
+const userRoutes = require('./routes/user');
+
+
+const app = express();
+
+app.use(helmet());
+
+db.sequelize
+  .sync()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+app.use(cors());
+app.use(express.json());
+
+// simple route
+app.use("/api", userRoutes);
+//app.use("/api/posts", postRoutes);
+//app.use("/api/comments", commentRoutes);
+
+//Set the images path.
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;
