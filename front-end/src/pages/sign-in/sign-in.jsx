fdaee4b1bbbc98bda0e21b994cfb22586e30efea
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-08-19 19:38:44 +0200

    [Add] SignIn Function

--- front-end/src/pages/sign-in/sign-in.jsx
+++ front-end/src/pages/sign-in/sign-in.jsx
@@ -1,42 +1,119 @@
-import React from "react";
-import "./sign-in.scss";
+import { React } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 import TextField from "@material-ui/core/TextField";
+import ErrorIcon from "@material-ui/icons/Error";
+
+import "./style.scss";
 import Nav from "../../components/header/logPage/index";
-import SignInbtn from "../../components/buttons/btn-signin/index"
+
+function getResponse(response) {
+  const notifySuccess = () => toast.success("Connexion en cours !");
+  const notifyError = () => toast.error("Une erreur est survenu !");
+  const notifyErrorPassword = () => toast.error("Mot de passe incorrecte !");
+
+  if (response.status === 200) {
+    notifySuccess();
+    setTimeout(function () {
+      window.location.href = "/feed";
+    }, 3500);
+  } else {
+    if (response.status === 401) {
+      notifyErrorPassword();
+    } else {
+      notifyError();
+    }
+  }
+  console.log(response.status);
+}
 
 export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <div>
       <Nav />
       <div className="main">
-        <div className="information-container">
-          <h2>Nous sommes ravis de vous revoir,</h2>
-          <h3>
-            identifiez-vous afin de retrouver votre communauté professionnelle.
-          </h3>
-          <div className="input-container">
-            <TextField
-              className="emailInput"
-              id="outlined-password-input"
-              label="Email"
-              type="email"
-              variant="outlined"
-            />
-            <TextField
-              className="passwordInput"
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              variant="outlined"
-            />
-          </div>
-          <div className="sign-in-Container">
-            <SignInbtn className="signBtn"/>
-          </div>
+        <div className="title-container">
+          <h2>Nous sommes ravis de vous revoir</h2>
+        </div>
+        <div className="input-container">
+          <form
+            onSubmit={handleSubmit((data) => {
+              fetch("http://localhost:3000/api/auth/signin", {
+                method: "POST",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+              }).then((response) => {
+                getResponse(response);
+                return response
+                  .json()
+                  .then((data) => {
+                    console.log(data);
+                    return data;
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              });
+            })}
+          >
+            <div className="input">
+              <TextField
+                className="email"
+                id="email"
+                label="Email*"
+                type="email"
+                variant="outlined"
+                margin="normal"
+                {...register("email", {
+                  required: "Ce champ est obligatoire",
+                })}
+              />
+              {errors.email && (
+                <p>
+                  <ErrorIcon />
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            <div className="input">
+              <TextField
+                className="password"
+                id="password"
+                label="Mot de Passe*"
+                type="password"
+                variant="outlined"
+                margin="normal"
+                {...register("password", {
+                  required: "Ce champ est obligatoire",
+                  minLength: {
+                    value: 8,
+                    message:
+                      "Le mot de passe doit contenir au moins 8 caractères",
+                  },
+                })}
+              />
+              {errors.password && (
+                <p>
+                  <ErrorIcon />
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+
+            <button type="submit">Valider</button>
+          </form>
         </div>
       </div>
-      <footer></footer>
     </div>
   );
 }
