8b1e342c65afe2861a1957ee08fca126fbcb2368
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-08-19 19:37:51 +0200

    [Fix] Back End

--- back_end/app.js
+++ back_end/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(helmet());
 
 db.sequelize
-  .sync()
+  .sync(/*{ force: true }*/)
   .then((result) => {
     console.log(result);
   })
--- back_end/models/Users.model.js
+++ back_end/models/Users.model.js
@@ -2,31 +2,27 @@
 
 module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define("Users", {
-    gender: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     lastName: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     firstName: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     isAdmin: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
-      default: false,
+      defaultValue: false,
+      allowNull: false
     },
   });
   return Users;
