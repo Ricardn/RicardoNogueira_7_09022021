d4089355e33dafa37a16ff47aae48d0c75116c51
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-08-27 12:43:55 +0200

    [Add] Protected Routes

--- front-end/src/ProtectedRoute.js
+++ front-end/src/ProtectedRoute.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Redirect } from 'react-router';
 
+
 class ProtectedRoute extends React.Component {
 
     render() {
+        
         const Component = this.props.component;
         const isAuthenticated = localStorage.getItem("token");
        
